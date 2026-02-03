4ca4a556ff91c668dcbac3cc03badc26cdf69d8c
Wen <weyun0513@users.noreply.github.com>
2026-02-02 21:24:23 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // 加入這一行，確保路徑從根目錄開始
   server: {
     historyApiFallback: true
   },
